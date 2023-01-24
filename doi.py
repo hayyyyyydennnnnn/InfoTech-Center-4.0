b53c818e96c50f04815cd85d9b32bdbe29593167
Hayden coates <haydencoates837@gmail.com>
2023-01-24 11:54:11 -0800

    Update welcome message print statement

---
+++ doi.py
@@ -0,0 +1,11 @@
+#Programmer: Hayden Coates
+#Date: 1/24/23
+#Program: InfoTech 4.0
+
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfotechCenter 4.0")
+print("\nInfotech Center 4.0 OS is loading")
