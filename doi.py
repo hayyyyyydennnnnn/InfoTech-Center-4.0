963e6e84d9b306e46c090763e439ba46416e3859
Hayden coates <haydencoates837@gmail.com>
2023-01-24 12:01:24 -0800

    Add Time.Sleep Library

--- doi.py
+++ doi.py
@@ -7,5 +7,14 @@ Our Welcome Screen will start our program letting
 drivers know that the InfoTech Center 4.0 OS is loading
 """
 
+#Import Libraries Here
+import time
+
+timeToSleep = 2
+
 print("\n\nWelcome - InfotechCenter 4.0")
+time.sleep(timeToSleep)
 print("\nInfotech Center 4.0 OS is loading")
+
+
+
