6f63971e778ed06e09c9418373266b3148ea5372
Hayden coates <haydencoates837@gmail.com>
2023-01-30 10:32:10 -0800

    Change color and spacing

--- doi.py
+++ doi.py
@@ -12,10 +12,8 @@ import time
 import sys  # I imported the system library for further use in code.
 
 timeToSleep = 2
-
-print("\n\nWelcome - InfotechCenter 4.0")
+print("\n\n\033[1;33;40mWelcome - InfotechCenter 4.0\n")
 time.sleep(timeToSleep)
-print("\nInfotech Center 4.0 OS is loading")
 
 #print("\nInfotech Center 4.0 OS is loading")
 x = 0
@@ -23,11 +21,11 @@ a = 0
 
 while x != 20:
     x += 1
-    b = ("\033[1;33;40m Infotech Center 4.0 OS is loading" + "." * a)
+    b = ("\033[1;33;40mInfotech Center 4.0 OS is loading" + "." * a)
     sys.stdout.write('\r' + b)  # \r prints a carriage return first, so `b` is printed on top of the previous line.
     time.sleep(0.5)
     if a == 4:
         a = 0
     if x == 20:
-        print('\033[1;32;40m Done!')
+        print('\n\n\033[1;32;40mMission Accomplished')
 
